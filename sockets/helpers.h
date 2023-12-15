23a7b39a0f881512778e735c0055943a502160cc
masonmil <masonmil@umich.edu>
2023-12-15 15:07:54 -0500

    sockets example, helper.h

--- sockets/helpers.h
+++ sockets/helpers.h
@@ -6,58 +6,46 @@
 #include <sys/socket.h>  // getsockname()
 #include <unistd.h>      // stderr
 
-/**
- * Make a server sockaddr given a port.
- * Parameters:
- *		addr: 	The sockaddr to modify (this is a C-style function).
- *		port: 	The port on which to listen for incoming connections.
- * Returns:
- *		0 on success, -1 on failure.
- * Example:
- *		struct sockaddr_in server;
- *		int err = make_server_sockaddr(&server, 8888);
- */
+// make a server sockaddr given a port
 int make_server_sockaddr(struct sockaddr_in *addr, int port) {
-    // Step (1): specify socket family.
+    // specify socket family
+    addr->sin_family = AF_INET;
 
-    // Step (2): specify socket address (hostname).
+    // specify socket address (hostname)
+    addr->sin_addr.s_addr = INADDR_ANY;
 
-    // Step (3): Set the port value.
+    // set the port value
+    addr->sin_port = htons(port);
 
     return 0;
 }
 
-/**
- * Make a client sockaddr given a remote hostname and port.
- * Parameters:
- *		addr: 		The sockaddr to modify (this is a C-style function).
- *		hostname: 	The hostname of the remote host to connect to.
- *		port: 		The port to use to connect to the remote hostname.
- * Returns:
- *		0 on success, -1 on failure.
- * Example:
- *		struct sockaddr_in client;
- *		int err = make_client_sockaddr(&client, "141.88.27.42", 8888);
- */
+// make a client sockaddr given a remote hostname and port
 int make_client_sockaddr(struct sockaddr_in *addr, const char *hostname, int port) {
-    // Step (1): specify socket family.
+    // specify socket family
+    addr->sin_family = AF_INET;
 
-    // Step (2): specify socket address (hostname).
+    // specify socket address (hostname)
+    struct hostent *host = gethostbyname(hostname);
+    if (host == NULL) {
+        fprintf(stderr, "unknown host: %s\n", hostname);
+        return -1;
+    }
+    memcpy(&addr->sin_addr, host->h_addr, host->h_length);
 
-    // Step (3): Set the port value.
+    // set the port value
+    addr->sin_port = htons(port);
 
     return 0;
 }
 
-/**
- * Return the port number assigned to a socket.
- *
- * Parameters:
- * 		sockfd:	File descriptor of a socket
- *
- * Returns:
- *		The port number of the socket, or -1 on failure.
- */
+// return the port number assigned to a socket
 int get_port_number(int sockfd) {
-    return 0;  // Placeholder
+    struct sockaddr_in addr;
+    socklen_t length = sizeof(addr);
+    if (getsockname(sockfd, (struct sockaddr *)&addr, &length) == -1) {
+        perror("Error getting port of socket");
+        return -1;
+    }
+    return ntohs(addr.sin_port);
 }
