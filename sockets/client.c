9b3d21b99867ccad4aa384ccd692bf1bc87110b4
masonmil <masonmil@umich.edu>
2023-12-19 20:46:16 -0500

    client.c send_message

--- sockets/client.c
+++ sockets/client.c
@@ -11,15 +11,41 @@ static const int MAX_MESSAGE_SIZE = 256;
 
 // sends a string message to the server
 int send_message(const char *hostname, int port, const char *message) {
+    // check message length
+    const size_t message_len = strlen(message);
+    if (message_len > MAX_MESSAGE_SIZE) {
+        perror("Error: Message exceeds maximum length\n");
+        return -1;
+    }
+
     // create a socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
 
     // create a sockaddr_in to specify remote host and port
+    struct sockaddr_in addr;
+    if (make_client_sockaddr(&addr, hostname, port) == -1) {
+        return -1;
+    }
 
     // connect to remote server
+    if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        perror("Error connecting stream socket");
+        return -1;
+    }
 
     // send message to remote server
+    ssize_t sent = 0;
+    do {
+        const ssize_t n = send(sockfd, message + sent, message_len - sent, 0);
+        if (n == -1) {
+            perror("Error sending on stream socket");
+            return -1;
+        }
+        sent += n;
+    } while (sent < message_len);
 
     // close connection
+    close(sockfd);
 
     return 0;
 }
